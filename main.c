61f7a6830da1eb2b191f306d39e194702d20f496
Skay <40888285_Skay@users.noreply.github.com>
2024-05-29 02:12:09 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <winsock2.h>
+#include <string.h>
+#include <pthread.h>
+#include <io.h>
+
+void handleThread(void* x){
+    int* data = (int*)x;
+    int connection = data[0];
+    int playerNum = data[1];
+    int bytes_recieved;
+    char* readBuffer[1024];
+    while(true){
+        //read(connection,readBuffer,512);
+        bytes_recieved = recv(connection,readBuffer,512,0);
+        if(bytes_recieved > 0){
+            int* input = ((int*)readBuffer);
+            if(input[0] == 12323){
+                if(input[1] != 0){
+                    printf("player %d: ", playerNum);
+                    if((input[1] % 2) == 1)
+                        printf("left ");
+                    if((input[1]/2 % 2) == 1)
+                        printf("right ");
+                    if((input[1]/4 % 2) == 1)
+                        printf("forward ");
+                    printf("\n");
+                }
+            }
+        }
+        for(int i = 0; i < 512;i++)
+            readBuffer[0] = "\0";
+    }
+}
+
+
+int main() {
+
+
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(2278);
+    addr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
+
+    WSADATA wsaData;
+    int iResult;
+    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0) {
+        printf("WSAStartup failed: %d\n", iResult);
+        return 1;
+    }
+
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd == -1) { //-1 when socket fails to socket
+        printf("error: %s\n", strerror(errno));
+    }
+
+    if( bind(sockfd, &addr, sizeof(addr))){
+        printf("error: %s\n", strerror(errno));
+    }
+
+    if( listen(sockfd, 7)){
+        printf("error: %s\n", strerror(errno));
+    }
+
+    int playerNum = 0;
+    while(true){
+        struct sockaddr_in bingus;
+        int length = sizeof(bingus);
+        int connection = accept(sockfd, &bingus, &length);
+        int paramData[2] = {connection,playerNum};
+        printf("connection accepted: %d\n",connection,strerror(errno));
+        pthread_t thread_id;
+        pthread_create(&thread_id, NULL, handleThread, paramData);
+        playerNum++;
+    }
+    WSACleanup();
+    return 0;
+}
