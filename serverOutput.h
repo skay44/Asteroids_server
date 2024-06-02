a19c0e418bc3f86b5609993ae39a209b4fa87a58
skay44 <michal.1805@wp.pl>
2024-06-02 14:23:56 +0200

    aaa

--- serverOutput.h
+++ serverOutput.h
@@ -66,8 +66,10 @@ void sendPlayerDeleteData(vectorPlayerState * a, playerState* b){
 
 void sendYouDiedData(vectorPlayerState * a, playerState* b){
     int sendTo = b->connectionAddr;
+
     //tworzenie ramki do przeslania (ramka asteroid)
     for(int i = 0; i < a->size; i++){
+        printf("You died sent");
         deletus psf = {DELETUS_CODE, 4, a->arr[i].playerID};
         send(sendTo, (char*)&psf, sizeof(deletus), 0);
     }
